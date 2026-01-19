e997ea17a52422b8f5464b87053c79b848fac3f0
Szymonppk <szymon.palka@student.pk.edu.pl>
2026-01-19 20:32:32 +0100

    feat(recording, player and download): added recording options to frontend

--- frontend/src/components/layout/RecordingSection.js
+++ frontend/src/components/layout/RecordingSection.js
@@ -1,12 +1,11 @@
-import {useState, useEffect, useRef} from 'react';
+import { useState, useEffect, useRef } from 'react';
 import micImage from '../../images/recording/microphone.png';
 import soundwave from '../../images/recording/soundwave.png';
 import playIcon from '../../images/recording/play.png';
-import download from '../../images/recording/download.png';
+import downloadIcon from '../../images/recording/download.png';
 import save from '../../images/recording/save.svg';
 
-function RecordingSection()
-{
+function RecordingSection() {
     const [isRecording, setIsRecording] = useState(false);
     const [seconds, setSeconds] = useState(0);
     const [audioUrl, setAudioUrl] = useState(null);
@@ -15,13 +14,14 @@ function RecordingSection()
     const mediaRecorderRef = useRef(null);
     const audioChunksRef = useRef([]);
 
-    useEffect(()=>{
+    // Timer
+    useEffect(() => {
         let interval = null;
 
-        if(isRecording){
-            interval = setInterval(()=>{
+        if (isRecording) {
+            interval = setInterval(() => {
                 setSeconds((prevSeconds) => prevSeconds + 1);
-            },1000);
+            }, 1000);
         } else {
             clearInterval(interval);
         }
@@ -29,57 +29,127 @@ function RecordingSection()
         return () => clearInterval(interval);
     }, [isRecording]);
 
-    const formatTime = (totalSeconds) =>{
+
+    const formatTime = (totalSeconds) => {
         const minutes = Math.floor(totalSeconds / 60);
         const secs = totalSeconds % 60;
-        return `${minutes.toString().padStart(2,'0')}:${secs.toString().padStart(2,'0')}`;
+        return `${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
     };
 
-    const handleMicClick = () =>{
-        if(!isRecording)
-        {
+    const startRecording = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+            const mediaRecorder = new MediaRecorder(stream);
+            mediaRecorderRef.current = mediaRecorder;
+            audioChunksRef.current = [];
+
+            mediaRecorder.ondataavailable = (event) => {
+                if (event.data.size > 0) {
+                    audioChunksRef.current.push(event.data);
+                }
+            };
+            mediaRecorder.onstop = () => {
+                const audioBlob = new Blob(audioChunksRef.current, { type: 'audio/wav' });
+                setAudioBlob(audioBlob);
+
+                const audioUrl = URL.createObjectURL(audioBlob);
+                setAudioUrl(audioUrl);
+
+                stream.getTracks().forEach(track => track.stop());
+            };
+
+            mediaRecorder.start();
+            setIsRecording(true);
             setSeconds(0);
+            setAudioUrl(null);
+        } catch (err) {
+            console.error('Couldn\'t access microphone', err);
+            alert('Couldn\'t access microphone');
         }
-        setIsRecording(!isRecording);
     };
 
-    return(
+    const stopRecording = () => {
+        if (mediaRecorderRef.current) {
+            mediaRecorderRef.current.stop();
+            setIsRecording(false);
+        }
+    };
+
+
+    const handleMicClick = () => {
+        if (isRecording) {
+            stopRecording()
+        }
+        else{
+            startRecording();
+        }
+        
+    };
+
+    const handlePlayAudio = () => {
+        if(audioUrl)
+        {
+            const audio = new Audio(audioUrl);
+            audio.play();
+        } else{
+            alert('Record something first 🎤');
+        }
+    };
+
+    const handleDownloadAudio = () => {
+        if(audioUrl)
+        {
+            const link = document.createElement('a');
+            link.href = audioUrl;
+            link.download = 'recording.wav';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    };
+
+    return (
         <div className='w-full flex-1 flex items-center justify-center flex-col md:flex-row flex-wrap gap-6 md:gap-3 p-4'>
-            
+
             <div className='flex flex-col items-center justify-center gap-2'>
                 <img
                     src={micImage}
                     alt='microphone'
-                    onClick={handleMicClick} 
+                    onClick={handleMicClick}
                     className={`w-[20vh] h-[20vh] cursor-pointer transition-transform hover:scale-125 ${isRecording ? 'scale-110 drop-shadow-xl animate-pulse' : ''}`}
                 />
-                
+
                 {isRecording && <div className={`text-2xl font-mono font-bold text-gray-700 transition-opacity duration-300 ${isRecording || seconds > 0 ? 'opacity-100' : 'opacity-0'}`}>
                     {formatTime(seconds)}
                 </div>}
             </div>
-            <img 
+            <img
                 src={soundwave}
                 className='w-[40vh] h-[20vh]'
-           
-           />
-           <div className='flex flex-row gap-8'>
-           <img 
-                src={playIcon}
-                alt='microphone'
-                className='w-[10vh] h-[10vh] cursor-pointer  transition-transform hover:scale-125'   
+
             />
-            <img 
-                src={download}
-                className='w-[10vh] h-[10vh] cursor-pointer transition-transform hover:scale-125'
-           
-           />
-            <img 
-                src={save}
-                className='w-[10vh] h-[10vh] cursor-pointer transition-transform hover:scale-125'
-           
-           />
-           </div>
+            <div className='flex flex-row gap-8'>
+                <img
+                    src={playIcon}
+                    onClick={handlePlayAudio}
+                    alt='microphone'
+                    className='w-[10vh] h-[10vh] cursor-pointer  transition-transform hover:scale-125'
+                />
+                <img
+                    src={downloadIcon}
+                    onClick={handleDownloadAudio}
+                    alt='download'
+                    className='w-[10vh] h-[10vh] cursor-pointer transition-transform hover:scale-125'
+
+                />
+                <img
+                    src={save}
+                    alt='save'
+                    className='w-[10vh] h-[10vh] cursor-pointer transition-transform hover:scale-125'
+
+                />
+            </div>
 
 
         </div>
